bf3bf7b69a44ac3178c5a8b917ad4c5fdfba0c6b
laurent <laurent.mazare@gmail.com>
2023-05-07 20:04:44 +0100

    Add a control flow test.

---
+++ tests/control_flow_tests.rs
@@ -0,0 +1,27 @@
+use xla::{ElementType, Result};
+
+#[test]
+fn while_op() -> Result<()> {
+    let client = xla::PjRtClient::cpu()?;
+    let builder = xla::XlaBuilder::new("test");
+    let cond = {
+        let builder = xla::XlaBuilder::new("cond");
+        let x = builder.parameter(0, i32::PRIMITIVE_TYPE, &[], "x")?;
+        x.le(&builder.constant_r0(10i32)?)?.build()?
+    };
+    let body = {
+        let builder = xla::XlaBuilder::new("cond");
+        let x = builder.parameter(0, i32::PRIMITIVE_TYPE, &[], "x")?;
+        (x + builder.constant_r0(1i32)?)?.build()?
+    };
+    let init = builder.constant_r0(0i32)?;
+    let w = xla::XlaOp::while_(cond, body, init)?;
+    let computation = w.build()?;
+    let result = client.compile(&computation)?;
+    let result = result.execute::<xla::Literal>(&[])?;
+    let result = result[0][0].to_literal_sync()?;
+    assert_eq!(result.element_count(), 1);
+    assert_eq!(result.shape()?, xla::Shape::new::<i32>(vec![]));
+    assert_eq!(result.to_vec::<i32>()?, [11]);
+    Ok(())
+}
