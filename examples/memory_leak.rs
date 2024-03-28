3a748a59ce513b84204af8069a9f82685a02541f
Eben Kadile <eben.kadile24@gmail.com>
2024-03-28 12:10:12 +0100

    weird seg fault with memory leak example

--- examples/memory_leak.rs
+++ examples/memory_leak.rs
@@ -8,12 +8,12 @@ fn main() -> Result<()> {
     let x = builder.parameter(0, f32::TY, &[1000000], "x")?;
     let comp = x.build()?;
     let exe = comp.compile(&client)?;
-    let mut x_val = xla::Literal::vec1(&[0f32; 1000000]);
+    let x_val = xla::Literal::vec1(&[0f32; 1000000]);
+    let mut x_buffer = client.buffer_from_host_literal(Some(&client.devices()[0]), &x_val)?;
     let mut count = 0;
     loop {
-        let xla_buffers = exe.execute(&[&x_val])?;
-        let literal_out = xla_buffers[0][0].to_literal_sync()?;
-        x_val = literal_out;
+        let mut xla_buffers = exe.execute_b(&[&x_buffer])?;
+        //x_buffer = xla_buffers.pop().unwrap().pop().unwrap();
         println!("{}", count);
         count += 1;
         //Ok(());
